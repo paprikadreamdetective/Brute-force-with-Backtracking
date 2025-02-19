52648ce17327b9069db0a9877794a3f025128188
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2025-02-18 18:12:06 -0600

    🎨 New class CifradoVigenere is up
    New class CifradoVigenere is up

--- cifrado_vigenere.py
+++ cifrado_vigenere.py
@@ -1,20 +1,30 @@
-def cifrar_vigenere(texto, clave):
-    texto = texto.upper()  # Convertimos todo a mayúsculas
-    clave = clave.upper()
-    texto_len = len(texto)
-    clave_len = len(clave)
-    
-    nueva_clave = ""  # Generar la clave extendida
-    for i in range(texto_len):
-        nueva_clave += clave[i % clave_len]
-    
-    cifrado_texto = ""
-    for i in range(texto_len):
-        cifrado_texto += chr(((ord(texto[i]) + ord(nueva_clave[i]) - 2 * ord('A')) % 26) + ord('A'))
-    
-    print("Texto cifrado:", cifrado_texto)
+class CifradoVigenere:
+    def __init__() -> None:
+        _text = ''
+        _true_key = ''
 
-# Prueba con los mismos datos
-texto = "HOLA"
-clave = "CLAVE"
-cifrar_vigenere(texto, clave)
+    def cifrar(self, texto: str, clave: str):
+        texto = texto.upper()  # Convertimos todo a mayúsculas
+        clave = clave.upper()
+        texto_len = len(texto)
+        clave_len = len(clave)
+        nueva_clave = ""  # Generar la clave extendida
+        for i in range(texto_len):
+            nueva_clave += clave[i % clave_len]
+        cifrado_texto = ""
+        for i in range(texto_len):
+            cifrado_texto += chr(((ord(texto[i]) + ord(nueva_clave[i]) - 2 * ord('A')) % 26) + ord('A'))
+        print("Texto cifrado:", cifrado_texto)
+
+    def descifrar(self, cifrado: str, clave: str):
+        cifrado = cifrado.upper()
+        clave = clave.upper()
+        texto_len = len(cifrado)
+        clave_len = len(clave)
+        nueva_clave = ""
+        for i in range(texto_len):
+            nueva_clave += clave[i % clave_len]
+        texto_descifrado = ""
+        for i in range(texto_len):
+            texto_descifrado += chr(((ord(cifrado[i]) - ord(nueva_clave[i]) + 26) % 26) + ord('A'))
+        return texto_descifrado
